80fcf434f23bf9be7ec009a92c7eb71a8c373091
nopiarahman <nopiarahman93@gmail.com>
2022-09-25 11:26:45 +0700

    add berita & kategori models

---
+++ database/migrations/2022_09_25_041049_create_berita_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Kategori;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBeritaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Kategori::class);
+            $table->string('judul');
+            $table->string('foto');
+            $table->date('tanggal');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita');
+    }
+}
